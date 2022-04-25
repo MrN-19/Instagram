c939ce9016c8d34223e9798719325a95c951b2ce
unknown <mohammadreze.mrn.29@gmail.com>
2022-04-25 23:48:33 +0430

    impeliment direct in project

--- direct/urls.py
+++ direct/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import share_post_direct
+from .views import share_post_direct,direct,get_messages
 app_name = "direct"
 urlpatterns = [
     path("share",share_post_direct,name="sharepost"),
+    path("direct",direct,name="direct"),
+    path("get-messages",get_messages,name="getmessages"),
+    
 ]
--- direct/views.py
+++ direct/views.py
@@ -7,6 +7,8 @@ from django.contrib.auth.models import User
 from .models import DirectMessage
 from setting.models import AppInfo
 from django.contrib import messages
+from django.db.models import Q
+from django.core.serializers import serialize
 
 def check_user_exists(usernames:list) -> bool:
     db_users = User.objects.all()
@@ -16,8 +18,19 @@ def check_user_exists(usernames:list) -> bool:
             res = False
     return res
 
+@login_required(login_url="account:login")
+@require_GET
+def direct(request):
+    current_user = request.user
+    direct_messages = DirectMessage.objects.filter(Q(sender = current_user)|Q(reciver = current_user)).order_by("-send_date")
+    data = {
+        "direct_messages" : direct_messages
+    }
+    return render(request,"direct/direct.html",data)
+
 
 @login_required(login_url="account:login")
+@require_POST
 def share_post_direct(request):
     postid = request.POST.get("postid")
     post = Post.objects.filter(code = postid).first()
@@ -41,3 +54,22 @@ def share_post_direct(request):
         "status" : False,"message" : "Not Found 404",
         "text" : "خطا در انجام عملیات"
     },status = 404)
+
+@login_required(login_url="account:login")
+@require_GET
+def get_messages(request):
+    try:
+        current_user = request.user
+        messages = DirectMessage.objects.filter(
+            Q(sender=current_user)|Q(reciver=current_user),
+        ).all()
+        messages = serialize(messages)
+        return JsonResponse({
+            "status" : True,"message" : "Success 200","data" : messages
+        },status = 200)
+    except:
+        pass
+    return JsonResponse({
+        "status" : False,"message" : "403 Bad Request","text" : "خطا در انجام عملیات"
+    })
+    
